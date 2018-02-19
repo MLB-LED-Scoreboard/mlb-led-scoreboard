9b2754f25920a5ea505066038f73a100a9fcd541
Andrew Bowler <bowlerandrewj@gmail.com>
2018-02-19 14:24:14 -0600

    Heavy refactor of the game renderer, add some docs

--- renderers/games.py
+++ renderers/games.py
@@ -7,65 +7,101 @@ import ledcolors.scoreboard
 import math
 import time
 
-FIFTEEN_SECONDS = 15
-FIFTEEN_MINUTES = 900
+# Times measured in seconds
+FIFTEEN_SECONDS = 15.0
+FIFTEEN_MINUTES = 900.0
 
-def render(matrix, canvas, games, args):
-  # Get the game to start on. If the provided team does not have a game today,
-  # or the team name isn't provided, then the first game in the list is used.
-  game_idx = 0
-  if args.team:
-    game_idx = next(
-        (i for i, game in enumerate(games) if game.away_team ==
-         args.team or game.home_team == args.team), 0
-    )
-  game = games[game_idx]
+# Refresh rates measured in seconds
+PREGAME_RATE = 0.2
+SCOREBOARD_RATE = FIFTEEN_SECONDS
 
-  canvas.Fill(*ledcolors.scoreboard.fill)
-  scrolling_text_pos = canvas.width
-  starttime = original_start_time = time.time()
+# Game statuses
+PRE_GAME = 'PRE_GAME'
 
-  while True:
-    endtime = time.time()
-    delta = endtime - starttime
-    full_delta = endtime - original_start_time
-    if full_delta >= FIFTEEN_MINUTES:
-      return
-    
-    refresh_rate = 0.2 if game.game_status == 'PRE_GAME' else 15.0
+class GameRenderer:
+  """An object that loops through and renders a list of games.
 
-    new_scrolling_text_pos = __refresh_scoreboard(canvas, game, scrolling_text_pos)
-    if new_scrolling_text_pos is not None:
-      scrolling_text_pos = new_scrolling_text_pos
+  Properties:
+    matrix                     - An instance of RGBMatrix
+    canvas                     - The canvas associated with the matrix
+    games                      - The list of games to render
+    args                       - Any supplied command line arguments
+    current_scrolling_text_pos - The current position of the probable starting
+                                 pitcher text for pregames.
+    creation_time              - The time at which this GameRender was created.
+  """
 
-    canvas = matrix.SwapOnVSync(canvas)
-    
-    # Refresh the board every 15 seconds and rotate the games
-    # if the command flag is passed
-    time.sleep(refresh_rate - ((delta) % refresh_rate))
-    canvas.Fill(*ledcolors.scoreboard.fill)
+  def __init__(self, matrix, canvas, games, args):
+    """Initializes a GameRender
+    """
+    self.matrix = matrix
+    self.canvas = canvas
+    self.games = games
+    self.args = args
+    self.current_scrolling_text_pos = self.canvas.width
+    self.creation_time = time.time()
 
-    if args.rotate and delta >= FIFTEEN_SECONDS:
-      starttime = time.time()
-      scrolling_text_pos = canvas.width
-      game_idx = bump_counter(game_idx, games)
-      game = games[game_idx]
+  def render(self):
+    """Renders a game or games depending on the configuration.
+    Infinitely loops up to 15 minutes before a refresh of the list of games
+    is required.
+    """
+    self.canvas.Fill(*ledcolors.scoreboard.fill)
+    current_game_index = self.__get_game_from_args()
+    game = self.games[current_game_index]
+    starttime = time.time()
 
-def __refresh_scoreboard(canvas, game, scrolling_text_pos):
-  renderer = {}
-  if game.game_status == 'PRE_GAME':
-    pregame = Pregame(game)
-    renderer = PregameRenderer(canvas, pregame, scrolling_text_pos)
+    while True:
+      endtime = time.time()
+      if endtime - self.creation_time >= FIFTEEN_MINUTES:
+        return
+      time_delta = endtime - starttime
 
-    current_scrolling_text_pos = renderer.render()
+      self.__refresh_game(game)
+      refresh_rate = PREGAME_RATE if game.game_status == PRE_GAME else SCOREBOARD_RATE
+      time.sleep(refresh_rate - ((time_delta) % refresh_rate))
+      self.canvas.Fill(*ledcolors.scoreboard.fill)
 
-    scrolling_text_pos -= 1
-    if scrolling_text_pos + current_scrolling_text_pos < 0:
-      scrolling_text_pos = canvas.width
-    return scrolling_text_pos
-  else:
-    scoreboard = Scoreboard(game)
-    if not scoreboard.game_data:
-      return
-    renderer = ScoreboardRenderer(canvas, scoreboard)
-    renderer.render()
+      # TODO: https://github.com/ajbowler/mlb-led-scoreboard/issues/30
+      # The time_delta comparison will need to change depending on scrolling text size
+      if self.args.rotate and time_delta >= FIFTEEN_SECONDS:
+        starttime = time.time()
+        self.current_scrolling_text_pos = self.canvas.width
+        current_game_index = bump_counter(current_game_index, self.games)
+        game = self.games[current_game_index]
+
+  def __get_game_from_args(self):
+    """Returns the index of the game to render.
+    If a preferred team was provided in the configuration that index is
+    picked if it exists, otherwise the first game in the list is used.
+    """
+    game_idx = 0
+    if self.args.team:
+      game_idx = next(
+          (i for i, game in enumerate(self.games) if game.away_team ==
+           self.args.team or game.home_team == self.args.team), 0
+      )
+    return game_idx
+
+  def __refresh_game(self, game):
+    """Draws the provided game on the canvas."""
+    if game.game_status == PRE_GAME:
+      pregame = Pregame(game)
+      renderer = PregameRenderer(self.canvas, pregame, self.current_scrolling_text_pos)
+      self.__update_scrolling_text_pos(renderer.render())
+    else:
+      scoreboard = Scoreboard(game)
+      # TODO: https://github.com/ajbowler/mlb-led-scoreboard/issues/13
+      # Render something else if a game's data is incomplete
+      if not scoreboard.game_data:
+        return
+      ScoreboardRenderer(self.canvas, scoreboard).render()
+    self.canvas = self.matrix.SwapOnVSync(self.canvas)
+
+  def __update_scrolling_text_pos(self, new_pos):
+    """Updates the position of the probable starting pitcher text."""
+    pos_after_scroll = self.current_scrolling_text_pos - 1
+    if pos_after_scroll + new_pos < 0:
+      self.current_scrolling_text_pos = self.canvas.width
+    else:
+      self.current_scrolling_text_pos = pos_after_scroll
--- renderers/pregame.py
+++ renderers/pregame.py
@@ -14,7 +14,7 @@ class Pregame:
     self.__render_matchup()
     self.__render_start_time()
     return self.__render_probable_starters()
-  
+
   def __render_matchup(self):
     away_text = '{:>3s}'.format(self.game.game_data['away_team'])
     home_text = '{:3s}'.format(self.game.game_data['home_team'])
@@ -22,11 +22,11 @@ class Pregame:
     graphics.DrawText(self.canvas, self.font, 1, 6, self.text_color, away_text)
     graphics.DrawText(self.canvas, self.font, 15, 6, self.text_color, '@')
     graphics.DrawText(self.canvas, self.font, 20, 6, self.text_color, home_text)
-  
+
   def __render_start_time(self):
     time_text = '{:7s}'.format(self.game.game_data['time'])
     graphics.DrawText(self.canvas, self.font, 5, 13, self.text_color, self.game.game_data['time'])
-  
+
   def __render_probable_starters(self):
     pitchers_text = self.game.game_data['away_pitcher'] + ' vs ' + self.game.game_data['home_pitcher']
     return graphics.DrawText(self.canvas, self.font, self.probable_starter_pos, 25, self.text_color, pitchers_text)
