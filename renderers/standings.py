bb7132422a5720f2d53a93576709264e6d0d9184
Steve Ehrenberg <tehdnite@gmail.com>
2018-02-20 11:05:15 +0000

    Adjusted the standings to fit a W or L to the right to make it more readable

--- renderers/standings.py
+++ renderers/standings.py
@@ -13,15 +13,18 @@ def render(matrix, canvas, division):
   starttime = time.time()
   while True:
     offset = 6
+    graphics.DrawText(canvas, font, 28, offset, text_color, stat.upper())
     for team in division.teams:
       abbrev = '{:3s}'.format(team.team_abbrev)
-      text = '%s %s' % (abbrev, getattr(team, stat))
-      graphics.DrawText(canvas, font, 1, offset, text_color, text)
+      team_text = '%s' % abbrev
+      stat_text = '%s' % getattr(team, stat)
+      graphics.DrawText(canvas, font, 1 , offset, text_color, team_text)
+      graphics.DrawText(canvas, font, 15, offset, text_color, stat_text)
 
       for x in range(0, canvas.width):
         canvas.SetPixel(x, offset, *ledcolors.standings.divider)
       for y in range(0, canvas.height):
-        canvas.SetPixel(14, y, *ledcolors.standings.divider)
+        canvas.SetPixel(13, y, *ledcolors.standings.divider)
       offset += 6
 
     matrix.SwapOnVSync(canvas)
