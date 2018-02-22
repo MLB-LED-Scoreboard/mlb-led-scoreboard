80f4d45218a7696d8f2e623631ceaf49031d32b2
Steve Ehrenberg <tehdnite@gmail.com>
2018-02-22 10:35:04 +0000

    Manually abbreviate the team names if we fail to find the overview

--- data/pregame.py
+++ data/pregame.py
@@ -8,12 +8,20 @@ class Pregame:
   def __pregame_data(self):
     # The overview API is used to get the teams' abbreviations
     # as opposed to the full names
-    overview = mlbgame.overview(self.game.game_id)
-
     game_data = {}
-    game_data['away_team'] = overview.away_name_abbrev
-    game_data['home_team'] = overview.home_name_abbrev
+
+    try:
+      overview = mlbgame.overview(self.game.game_id)
+      game_data['away_team'] = overview.away_name_abbrev
+      game_data['home_team'] = overview.home_name_abbrev
+    except ValueError:
+      # We can't find the overview page. Let's just abbreviate the teams manually
+      game_data['away_team'] = self.game.away_team[:3]
+      game_data['home_team'] = self.game.home_team[:3]
+      game_data['error'] = "Game Data Not Found"
+
     game_data['time'] = self.game.game_start_time
     game_data['away_pitcher'] = self.game.p_pitcher_away or 'TBD'
     game_data['home_pitcher'] = self.game.p_pitcher_home or 'TBD'
+
     return game_data
