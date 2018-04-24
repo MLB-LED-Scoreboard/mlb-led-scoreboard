b5e624bcf638c95ee69b416b9fcc580cf58b47b2
Steve Ehrenberg <tehdnite@gmail.com>
2018-04-24 04:06:17 -0500

    Flush after each log statement to help with systemd journal realtime updates

--- debug.py
+++ debug.py
@@ -1,5 +1,6 @@
 import data.scoreboard_config
 import time
+import sys
 
 debug_enabled = False
 
@@ -10,9 +11,11 @@ def set_debug_status(config):
 def log(text):
 	if debug_enabled:
 		print("DEBUG ({}): {}".format(__timestamp(), text))
+		sys.stdout.flush()
 
 def error(text):
 	print("ERROR ({}): {}".format(__timestamp(), text))
+	sys.stdout.flush()
 
 def __timestamp():
 	return time.strftime("%H:%M:%S", time.localtime())
