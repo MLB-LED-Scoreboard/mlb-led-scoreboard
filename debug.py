faa1bf29d161912047f9117a494f561d1a6c54ff
Steve Ehrenberg <tehdnite@gmail.com>
2018-04-27 19:36:41 -0500

    Add basic script info when script starts (#132)

--- debug.py
+++ debug.py
@@ -8,14 +8,19 @@ def set_debug_status(config):
 	global debug_enabled
 	debug_enabled = config.debug_enabled
 
+def __debugprint(text):
+	print(text)
+	sys.stdout.flush()
+
 def log(text):
 	if debug_enabled:
-		print("DEBUG ({}): {}".format(__timestamp(), text))
-		sys.stdout.flush()
+		__debugprint("DEBUG ({}): {}".format(__timestamp(), text))
 
 def error(text):
-	print("ERROR ({}): {}".format(__timestamp(), text))
-	sys.stdout.flush()
+	__debugprint("ERROR ({}): {}".format(__timestamp(), text))
+
+def info(text):
+	__debugprint("INFO ({}): {}".format(__timestamp(), text))
 
 def __timestamp():
 	return time.strftime("%H:%M:%S", time.localtime())
