055e2c7f269b99db3bf2f25e9ab6ce1bff64685b
Andrew Bowler <bowlerandrewj@gmail.com>
2018-04-29 18:39:45 -0500

    Lock down the mlbgame version until fixes are released on their end

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ sudo pip install -e python/
 cd ../../
 echo "Installing required dependencies. This may take some time (10 minutes-ish)..."
 sudo apt-get install libxml2-dev libxslt-dev
-sudo pip install mlbgame pytz tzlocal
+sudo pip install mlbgame==2.4.2 pytz tzlocal
 make
 echo "Installation complete. Play around with the examples in matrix/bindings/python/samples, or make your config file and turn on your scoreboard!"
