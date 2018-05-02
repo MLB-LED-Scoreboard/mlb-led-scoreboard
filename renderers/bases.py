1524f619f559edd7babb7c598ebc41d3aa0bea7b
Joel Guth <joel.guth@gmail.com>
2018-05-01 22:14:16 -0500

    Removed static settings from files for new json options

--- renderers/bases.py
+++ renderers/bases.py
@@ -32,31 +32,21 @@ class BasesRenderer:
 
   def __render_base_outline(self, base):
     # Hollow diamonds are a popular homework problem but IDGAF
-
-    self.canvas.SetPixel(base['x'] - 5, base['y'], *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] - 4, base['y'] - 1, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] - 4, base['y'] + 1, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] - 3, base['y'] - 2, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] - 3, base['y'] + 2, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] - 2, base['y'] - 3, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] - 2, base['y'] + 3, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] - 1, base['y'] - 4, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] - 1, base['y'] + 4, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'], base['y'] - 5, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'], base['y'] + 5, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] + 1, base['y'] - 4, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] + 1, base['y'] + 4, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] + 2, base['y'] - 3, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] + 2, base['y'] + 3, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] + 3, base['y'] - 2, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] + 3, base['y'] + 2, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] + 4, base['y'] - 1, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] + 4, base['y'] + 1, *ledcolors.scoreboard.text)
-    self.canvas.SetPixel(base['x'] + 5, base['y'], *ledcolors.scoreboard.text)
-
+    self.canvas.SetPixel(base['x'] - 3, base['y'], *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'] - 2, base['y'] - 1, *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'] - 2, base['y'] + 1, *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'] - 1, base['y'] - 2, *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'] - 1, base['y'] + 2, *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'], base['y'] - 3, *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'], base['y'] + 3, *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'] + 1, base['y'] - 2, *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'] + 1, base['y'] + 2, *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'] + 2, base['y'] - 1, *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'] + 2, base['y'] + 1, *ledcolors.scoreboard.text)
+    self.canvas.SetPixel(base['x'] + 3, base['y'], *ledcolors.scoreboard.text)
 
   def __render_baserunner(self, base):
-    offset = 3
+    offset = 2
     for x in range(-offset, offset + 1):
       for y in range(-offset, offset + 1):
         if abs(x) == offset and abs(y) == offset:
--- renderers/outs.py
+++ renderers/outs.py
@@ -23,20 +23,15 @@ class OutsRenderer:
       self.__render_out_circle(out_px[out])
       # Fill in the circle if that out has occurred
       if (self.outs.number > out):
-        for x in range (-2,2):
-          for y in range (-2,2):
-            self.canvas.SetPixel(out_px[out]['x'] + x, out_px[out]['y'] + y, *ledcolors.scoreboard.text)
+        self.canvas.SetPixel(
+            out_px[out]['x'], out_px[out]['y'], *ledcolors.scoreboard.text)
 
   def __render_out_circle(self, out):
-    offset = 2
+    offset = 1
     for x in range(-offset, offset + 1):
       for y in range(-offset, offset + 1):
         # The dead center is filled in only if that many outs has occurred, and happens above
         # after this circle is rendered
         if x == 0 and y == 0:
           continue
-        if x in [-1, 0, 1] and y in [-1, 0, 1]:
-          continue
-	#if x == 0 and y in [-1,1]:
-        #  continue
         self.canvas.SetPixel(out['x'] + x, out['y'] + y, *ledcolors.scoreboard.text)
