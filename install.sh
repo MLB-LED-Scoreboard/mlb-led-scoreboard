361f39512ff21275d0103efd94ab9b9443484dcd
Tommy Farley <twfarley@gmail.com>
2019-03-02 09:58:30 -0800

    Adding -n argument to skip config.json question
    Adding in -n argument to skip the config.json if statement. This will make it easier to run Shortcuts.

--- install.sh
+++ install.sh
@@ -18,18 +18,22 @@ sudo pip uninstall -y mlbgame
 sudo pip install git+git://github.com/ajbowler/mlbgame.git@#egg=mlbgame
 make
 echo "If you didn't see any errors above, everything should be installed!"
-echo -e "\nYou'll need a config.json file to customize your settings. If you are updating"
-echo "from an older version and you were required to run this install script again or"
-echo "this is a fresh install, it's recommended we make a fresh one right now."
-echo "This will create a brand new 'config.json' file with default values so edit this"
-echo -e "file with your own settings.\n"
-read -p "Would you like to do this now? [Y/n] " answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-    rm config.json
-    cp config.json.example config.json
-    echo -e "\nYou should now have a fresh config.json file you can customize with your own settings.\n"
+if [ -n "$1" ]; then
+    echo -e "\nSkipping config.json file creation"
 else
-    echo -e "\nIf you do not have a config.json, you can manually copy the config.json.example to config.json to customize settings.\n"
+    echo -e "\nYou'll need a config.json file to customize your settings. If you are updating"
+    echo "from an older version and you were required to run this install script again or"
+    echo "this is a fresh install, it's recommended we make a fresh one right now."
+    echo "This will create a brand new 'config.json' file with default values so edit this"
+    echo -e "file with your own settings.\n"
+    read -p "Would you like to do this now? [Y/n] " answer
+    if [ "$answer" != "${answer#[Yy]}" ] ;then
+        rm config.json
+        cp config.json.example config.json
+        echo -e "\nYou should now have a fresh config.json file you can customize with your own settings.\n"
+    else
+        echo -e "\nIf you do not have a config.json, you can manually copy the config.json.example to config.json to customize settings.\n"
+    fi
 fi
 chown pi:pi config.json
 echo "Installation complete! Play around with the examples in matrix/bindings/python/samples to make sure your matrix is working."
