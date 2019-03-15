86d0ae3a10c2f5fba6804bdd25ea528a065f636f
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2019-03-15 16:45:36 -0400

    Bug Fix
    Use global games variable on self

--- data/data.py
+++ data/data.py
@@ -222,7 +222,7 @@ class Data:
 
   def is_offday_for_preferred_team(self):
     if self.config.preferred_teams:
-      return not any([self.config.preferred_teams[0] in [game.away_team, game.home_team] for game in games])
+      return not any([self.config.preferred_teams[0] in [game.away_team, game.home_team] for game in self.games])
     else:
       return True
 
