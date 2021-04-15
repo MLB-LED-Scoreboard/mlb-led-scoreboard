ca9cffa3e0f756633e90276ca00978dcf03cf596
James D. Mordarski <jmordars@gmail.com>
2021-04-15 17:13:36 -0600

    updated install script

--- install.sh
+++ install.sh
@@ -13,7 +13,8 @@ git checkout master
 git fetch origin --prune
 git pull
 sudo apt-get install libxml2-dev libxslt-dev
-sudo pip install pytz tzlocal "feedparser<6.0.0" pyowm
+sudo pip install pytz tzlocal "feedparser<6.0.0"
+sudo pip install git+https://github.com/csparpa/pyowm.git@1a29c198d797456e2fbb780aaf89582448490c74
 sudo pip uninstall -y mlbgame
 sudo pip install git+git://github.com/ajbowler/mlbgame.git@#egg=mlbgame
 make
