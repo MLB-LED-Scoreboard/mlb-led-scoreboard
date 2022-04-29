022adabedad86bcd0f75eee5064ec5a84bf5050b
Nicholas Saraniti <nick@nirica.com>
2022-04-29 09:15:17 -0400

    Added PITCH_LONG and PITCH_SHORT configurable dict

---
+++ data/pitches.py
@@ -0,0 +1,45 @@
+#A list of mlb pitch types appearing in statcast
+#https://www.daktronics.com/en-us/support/kb/DD3312647
+#Dont change the index, but feel free to change 
+#the long descriptions
+
+PITCH_LONG = {
+    "AB": "Automatic Ball",
+    "AS": "Automatic Strike",
+    "CH": "Change-up",
+    "EP": "Eephus",
+    "FC": "Cutter",
+    "FF": "4 Seam Fastball",    #MLB default is "Four-Seam Fastball"
+    "FO": "Forkball",
+    "FS": "Splitter",
+    "FT": "2 Seam Fastball",    #MLB default is "Two-Seam Fastball"
+    "GY": "Gyroball",
+    "IN": "Intentional Ball",
+    "KC": "Knuckle Curve",
+    "KN": "Knuckleball",
+    "NP": "No Pitch",
+    "SC": "Screwball",
+    "SI": "Sinker",
+    "SL": "Slider",
+    "UN": "Unknown"
+} 
+PITCH_SHORT = {
+    "AB": "AB",
+    "AS": "AS",
+    "CH": "CH",
+    "EP": "EP",
+    "FC": "FC",
+    "FF": "FF",
+    "FO": "FO",
+    "FS": "FS",
+    "FT": "FT",
+    "GY": "GY",
+    "IN": "IN",
+    "KC": "KC",
+    "KN": "KN",
+    "NP": "NP",
+    "SC": "SC",
+    "SI": "SI",
+    "SL": "SL",
+    "UN": "UN"
+} 
