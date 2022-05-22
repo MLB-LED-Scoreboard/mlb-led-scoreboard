6c3d3c5fb26b0e8b79efff9d7c0d4a7050c15e77
Nicholas Saraniti <nick@nirica.com>
2022-05-22 17:32:15 -0400

    Updated list of long and short pitch types

--- data/pitches.py
+++ data/pitches.py
@@ -1,27 +1,33 @@
-#A list of mlb pitch types appearing in statcast
-#https://www.daktronics.com/en-us/support/kb/DD3312647
-#Dont change the index, but feel free to change 
-#the descriptions
+# A list of mlb pitch types appearing in statcast
+# https://www.daktronics.com/en-us/support/kb/DD3312647
+# https://developer.sportradar.com/docs/read/baseball/MLB_v7_with_Statcast#frequently-asked-questions
+# Dont change the index, but feel free to change 
+# the descriptions
 
 PITCH_LONG = {
-    "AB": "Automatic Ball",
-    "AS": "Automatic Strike",
+    "AB": "Auto Ball",  #MLB default is "Automatic Ball"
+    "AS": "Auto Strike",#MLB default is "Automatic Strike"
     "CH": "Change-up",
     "CU": "Curveball",
+    "CS": "Slow Curve",
     "EP": "Eephus",
     "FC": "Cutter",
-    "FF": "4 Seam Fastball",    #MLB default is "Four-Seam Fastball"
+    "FA": "Fastball",
+    "FF": "Fastball",    #MLB default is "Four-Seam Fastball"
+    "FL": "Slutter",
     "FO": "Forkball",
     "FS": "Splitter",
-    "FT": "2 Seam Fastball",    #MLB default is "Two-Seam Fastball"
+    "FT": "2 Seamer",    #MLB default is "Two-Seam Fastball"
     "GY": "Gyroball",
-    "IN": "Intentional Ball",
+    "IN": "Int Ball",    #MLB default is "Intentional Ball"
     "KC": "Knuckle Curve",
     "KN": "Knuckleball",
     "NP": "No Pitch",
+    "PO": "Pitchout",
     "SC": "Screwball",
     "SI": "Sinker",
     "SL": "Slider",
+    "SU": "Slurve",
     "UN": "Unknown"
 } 
 PITCH_SHORT = {
@@ -29,9 +35,12 @@ PITCH_SHORT = {
     "AS": "AS",
     "CH": "CH",
     "CU": "CU",
+    "CS": "CS",
     "EP": "EP",
     "FC": "FC",
+    "FA": "FA",
     "FF": "FF",
+    "FL": "FL",
     "FO": "FO",
     "FS": "FS",
     "FT": "FT",
@@ -40,8 +49,10 @@ PITCH_SHORT = {
     "KC": "KC",
     "KN": "KN",
     "NP": "NP",
+    "PO": "PO",
     "SC": "SC",
     "SI": "SI",
     "SL": "SL",
+    "SU": "SU",
     "UN": "UN"
 } 
