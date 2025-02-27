3e18a4ba12ca05acdd4c27eeb99b2346346919ae
Brian Ward <brianmward99@gmail.com>
2025-02-27 15:51:48 -0500

    Begin city connect support

--- data/scoreboard/team.py
+++ data/scoreboard/team.py
@@ -1,8 +1,32 @@
+import debug
+
+
 class Team:
-    def __init__(self, abbrev, runs, name, hits, errors, record):
+    def __init__(self, abbrev, runs, name, hits, errors, record, special_uniform):
         self.abbrev = abbrev
         self.runs = runs
         self.name = name
         self.hits = hits
         self.errors = errors
         self.record = record
+        self.special_uniform = special_uniform
+
+
+    def lookup_color(self, team_colors):
+        """
+        Given a team color object, this returns the colors for the team.
+
+        The result is guaranteed to have the 'home', 'accent', and 'text' keys.
+        Any missing data is filled in with the default colors.
+        If the team has a special uniform, the colors for that uniform are substuted
+        """
+        default_colors = team_colors.color("default")
+        try:
+            colors = team_colors.color(self.abbrev.lower())
+            if self.special_uniform is not None and self.special_uniform in colors:
+                colors |= colors[self.special_uniform]
+            return default_colors | colors
+
+        except KeyError:
+            debug.exception("No color found for team: {}".format(self.abbrev))
+            return default_colors
--- renderers/games/teams.py
+++ renderers/games/teams.py
@@ -6,29 +6,8 @@ RELATIVE = "relative"
 def render_team_banner(
     canvas, layout, team_colors, home_team, away_team, full_team_names, short_team_names_for_runs_hits, show_score,
 ):
-    default_colors = team_colors.color("default")
-
-    away_colors = __team_colors(team_colors, away_team.abbrev)
-    try:
-        away_team_color = away_colors["home"]
-    except KeyError:
-        away_team_color = team_colors.color("default.home")
-
-    home_colors = __team_colors(team_colors, home_team.abbrev)
-    try:
-        home_team_color = home_colors["home"]
-    except KeyError:
-        home_team_color = team_colors.color("default.home")
-
-    try:
-        away_team_accent = away_colors["accent"]
-    except KeyError:
-        away_team_accent = team_colors.color("default.accent")
-
-    try:
-        home_team_accent = home_colors["accent"]
-    except KeyError:
-        home_team_accent = team_colors.color("default.accent")
+    away_colors = away_team.lookup_color(team_colors)
+    home_colors = home_team.lookup_color(team_colors)
 
     bg_coords = {}
     bg_coords["away"] = layout.coords("teams.background.away")
@@ -39,17 +18,17 @@ def render_team_banner(
     accent_coords["home"] = layout.coords("teams.accent.home")
 
     for team in ["away", "home"]:
+        color = home_colors['home'] if team == "home" else away_colors['home']
         for x in range(bg_coords[team]["width"]):
             for y in range(bg_coords[team]["height"]):
-                color = away_team_color if team == "away" else home_team_color
                 x_offset = bg_coords[team]["x"]
                 y_offset = bg_coords[team]["y"]
                 canvas.SetPixel(x + x_offset, y + y_offset, color["r"], color["g"], color["b"])
 
     for team in ["away", "home"]:
+        color = home_colors['accent'] if team == "home" else away_colors['accent']
         for x in range(accent_coords[team]["width"]):
             for y in range(accent_coords[team]["height"]):
-                color = away_team_accent if team == "away" else home_team_accent
                 x_offset = accent_coords[team]["x"]
                 y_offset = accent_coords[team]["y"]
                 canvas.SetPixel(x + x_offset, y + y_offset, color["r"], color["g"], color["b"])
@@ -58,11 +37,11 @@ def render_team_banner(
         canvas, full_team_names, short_team_names_for_runs_hits, [home_team, away_team]
     )
 
-    away_name_end_pos = __render_team_text(canvas, layout, away_colors, away_team, "away", use_full_team_names, default_colors)
-    home_name_end_pos = __render_team_text(canvas, layout, home_colors, home_team, "home", use_full_team_names, default_colors)
+    away_name_end_pos = __render_team_text(canvas, layout, away_colors['text'], away_team, "away", use_full_team_names)
+    home_name_end_pos = __render_team_text(canvas, layout, home_colors['text'], home_team, "home", use_full_team_names)
 
-    __render_record_text(canvas, layout, away_colors, away_team, "away", default_colors, away_name_end_pos)
-    __render_record_text(canvas, layout, home_colors, home_team, "home", default_colors, home_name_end_pos)
+    __render_record_text(canvas, layout, away_colors['text'], away_team, "away", away_name_end_pos)
+    __render_record_text(canvas, layout, home_colors['text'], home_team, "home", home_name_end_pos)
 
     if show_score:
         # Number of characters in each score.
@@ -71,8 +50,8 @@ def render_team_banner(
             "hits": max(len(str(away_team.hits)), len(str(home_team.hits))),
             "errors": max(len(str(away_team.errors)), len(str(home_team.errors))),
         }
-        __render_team_score(canvas, layout, away_colors, away_team, "away", default_colors, score_spacing)
-        __render_team_score(canvas, layout, home_colors, home_team, "home", default_colors, score_spacing)
+        __render_team_score(canvas, layout, away_colors['text'], away_team, "away", score_spacing)
+        __render_team_score(canvas, layout, home_colors['text'], home_team, "home", score_spacing)
 
 
 def can_use_full_team_names(canvas, enabled, abbreviate_on_overflow, teams):
@@ -98,16 +77,9 @@ def can_use_full_team_names(canvas, enabled, abbreviate_on_overflow, teams):
     return False
 
 
-def __team_colors(team_colors, team_abbrev):
-    try:
-        team_colors = team_colors.color(team_abbrev.lower())
-    except KeyError:
-        team_colors = team_colors.color("default")
-    return team_colors
 
 
-def __render_team_text(canvas, layout, colors, team, homeaway, full_team_names, default_colors):
-    text_color = colors.get("text", default_colors["text"])
+def __render_team_text(canvas, layout, text_color, team, homeaway, full_team_names):
     text_color_graphic = graphics.Color(text_color["r"], text_color["g"], text_color["b"])
     coords = layout.coords("teams.name.{}".format(homeaway))
     font = layout.font("teams.name.{}".format(homeaway))
@@ -118,13 +90,12 @@ def __render_team_text(canvas, layout, colors, team, homeaway, full_team_names, 
 
     return (coords["x"] + (len(team_text) * font["size"]["width"]), coords["y"])
 
-def __render_record_text(canvas, layout, colors, team, homeaway, default_colors, origin):
+def __render_record_text(canvas, layout, text_color, team, homeaway, origin):
     if "losses" not in team.record or "wins" not in team.record:
         return
     if not layout.coords("teams.record").get("enabled", False):
         return
 
-    text_color = colors.get("text", default_colors["text"])
     text_color_graphic = graphics.Color(text_color["r"], text_color["g"], text_color["b"])
     coords = layout.coords("teams.record.{}".format(homeaway))
     font = layout.font("teams.record.{}".format(homeaway))
@@ -138,13 +109,12 @@ def __render_record_text(canvas, layout, colors, team, homeaway, default_colors,
 
     graphics.DrawText(canvas, font["font"], x, y, text_color_graphic, record_text)
 
-def __render_score_component(canvas, layout, colors, homeaway, default_colors, coords, component_val, width_chars):
+def __render_score_component(canvas, layout, text_color, homeaway, coords, component_val, width_chars):
     # The coords passed in are the rightmost pixel.
     font = layout.font(f"teams.runs.{homeaway}")
     font_width = font["size"]["width"]
     # Number of pixels between runs/hits and hits/errors.
     rhe_coords = layout.coords("teams.runs.runs_hits_errors")
-    text_color = colors.get("text", default_colors["text"])
     text_color_graphic = graphics.Color(text_color["r"], text_color["g"], text_color["b"])
     component_val = str(component_val)
     # Draw each digit from right to left.
@@ -158,13 +128,13 @@ def __render_score_component(canvas, layout, colors, homeaway, default_colors, c
     coords["x"] -= font_width * width_chars + rhe_coords["spacing"] - 1  # adjust coordinates for next score.
 
 
-def __render_team_score(canvas, layout, colors, team, homeaway, default_colors, score_spacing):
+def __render_team_score(canvas, layout, text_color, team, homeaway, score_spacing):
     coords = layout.coords(f"teams.runs.{homeaway}").copy()
     if layout.coords("teams.runs.runs_hits_errors")["show"]:
         __render_score_component(
-            canvas, layout, colors, homeaway, default_colors, coords, team.errors, score_spacing["errors"]
+            canvas, layout, text_color, homeaway, coords, team.errors, score_spacing["errors"]
         )
         __render_score_component(
-            canvas, layout, colors, homeaway, default_colors, coords, team.hits, score_spacing["hits"]
+            canvas, layout, text_color, homeaway, coords, team.hits, score_spacing["hits"]
         )
-    __render_score_component(canvas, layout, colors, homeaway, default_colors, coords, team.runs, score_spacing["runs"])
+    __render_score_component(canvas, layout, text_color, homeaway, coords, team.runs, score_spacing["runs"])
