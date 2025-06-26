8622010714b08a304bd3b62be0cc9f391e89a9ab
Nicholas Saraniti <nick@nirica.com>
2025-06-26 17:24:08 -0400

    Make API refresh rate configurable

--- data/config/__init__.py
+++ data/config/__init__.py
@@ -67,6 +67,7 @@ class Config:
         self.short_team_names_for_runs_hits = json["short_team_names_for_runs_hits"]
         self.pregame_weather = json["pregame_weather"]
         self.delay_in_10s_of_seconds = json["preferred_game_update_delay_in_10s_of_seconds"]
+        self.api_refresh_rate = json["api_refresh_rate"]
 
         self.debug = json["debug"]
         self.demo_date = json["demo_date"]
@@ -99,6 +100,7 @@ class Config:
         # Check the rotation_rates to make sure it's valid and not silly
         self.check_rotate_rates()
         self.check_delay()
+        self.check_api_refresh_rate()
 
     def check_preferred_teams(self):
         if not isinstance(self.preferred_teams, str) and not isinstance(self.preferred_teams, list):
@@ -123,6 +125,19 @@ class Config:
                 f" Truncating to {int(self.delay_in_10s_of_seconds)}"
             )
             self.delay_in_10s_of_seconds = int(self.delay_in_10s_of_seconds)
+    
+    def check_api_refresh_rate(self):
+        if self.api_refresh_rate < 3:
+            debug.warning(
+                "api_refresh_rate should be a positive integer greater than 2. Using default value of 10"
+            )
+            self.api_refresh_rate = 10
+        if self.api_refresh_rate != int(self.api_refresh_rate):
+            debug.warning(
+                "api_refresh_rate should be an integer."
+                f" Truncating to {int(self.api_refresh_rate)}"
+            )
+            self.api_refresh_rate = int(self.api_refresh_rate)
 
     def check_preferred_divisions(self):
         if not isinstance(self.preferred_divisions, str) and not isinstance(self.preferred_divisions, list):
