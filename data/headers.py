0135fc64683d1a5a2185cf9fe13be58f56012990
Nicholas Saraniti <nick@nirica.com>
2025-06-27 19:37:55 -0400

    added version number to user-agent

--- data/headers.py
+++ data/headers.py
@@ -1,11 +1,15 @@
 # Headers to be sent on each statsapi.get()
 # call.
 
+import version
+
 # TODO (NMS): Detect rpi hardware.  Inject gzip headers only for
 #             rpi's >=3 or emulator.
 
 
+
+
 API_HEADERS = {
     'Accept-Encoding': 'gzip',
-    'User-Agent': 'MLB-LED-Scoreboard',
+    'User-Agent': 'MLB-LED-Scoreboard/' + version.SCRIPT_VERSION,
 }
