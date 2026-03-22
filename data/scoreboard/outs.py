ba534ef3f17352cb0238b5149d24c4bcb97c3ca5
Brian Ward <brianmward99@gmail.com>
2026-03-22 11:54:59 -0400

    Move debug print inside game, typing cleanup

--- data/scoreboard/outs.py
+++ data/scoreboard/outs.py
@@ -1,6 +1,9 @@
-from data.game import Game
+from typing import TYPE_CHECKING
 
+
+if TYPE_CHECKING:
+    from data.game import Game
 
 class Outs:
-    def __init__(self, game: Game):
+    def __init__(self, game: "Game"):
         self.number = game.outs()
